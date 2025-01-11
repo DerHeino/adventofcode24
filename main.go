d746d150e14606e81c2f2edc76e6b5247ab18e79
DerHeino <chan.heinrich@protonmail.com>
2025-01-11 22:29:13 +0100

    Part 2: Implementation

--- main.go
+++ main.go
@@ -58,28 +58,20 @@ func retrieveSession() string {
 
 //////// Day 19 implementation ////////
 
-/*
- * -- Greedy approach --
- * Recursion probably
- * For each design
- *   Look for shortest pattern and remove that from design
- *     Repeat for shortened design
- *   Else go back to previous design and pick next shortest pattern
- *   If design is empty at the end success
- *   Else failure
- *
- * -- Reading --
- * Read first line into array of existing "patterns" should be sorted by length and letter
- * Read third line (second is empty) into array of wanted "designs"
- */
-
 type Patterns struct {
-	templates []string
-	blacklist []string
+	templates     []string
+	blacklist     []string
+	whitelist     []string
+	cache         map[string]int
+	possibilities int
 }
 
-func (p *Patterns) append(impossible string) {
+func (p *Patterns) appendToBlacklist(impossible string) {
 	p.blacklist = append(p.blacklist, impossible)
+}
+
+func (p *Patterns) appendToWhitelist(design string) {
+	p.whitelist = append(p.whitelist, design)
 }
 
 func parse(body []byte) {
@@ -105,48 +97,52 @@ func parse(body []byte) {
 		if len(patterns[i]) == len(patterns[j]) {
 			return patterns[i] < patterns[j]
 		}
-		return len(patterns[i]) > len(patterns[j])
+		return len(patterns[i]) < len(patterns[j])
 	})
 
 	designs := strings.Split(strings.Trim(string(designsByte), "\n"), "\n")
 
-	//fmt.Printf("%v\n", patterns)
-	verifyDesigns(designs, patterns)
+	possibleDesigns := verifyDesigns(designs, patterns)
+	calculatePossibilitiesForAll(&possibleDesigns)
 }
 
-func verifyDesigns(designs []string, templates []string) {
-	patterns := Patterns{templates, make([]string, 0, 1024)}
+func verifyDesigns(designs []string, templates []string) Patterns {
+	patterns := Patterns{templates, make([]string, 0, 1024), make([]string, 0, 400), make(map[string]int, 4000), 0}
 	success := 0
 
 	for _, design := range designs {
-		if designPossible(design, &patterns) {
+		if designPossible(design, &patterns, 0) {
+			patterns.appendToWhitelist(design)
 			success++
 		} else {
-			fmt.Printf("[ FAILURE ] : \"%s\"\n", design)
+			patterns.appendToBlacklist(design)
 		}
 	}
 
 	fmt.Printf("Possible designs: %d/%d\n", success, len(designs))
+	return patterns
 }
 
 // verify if containsPatterns fails, continue until no patterns are left (that should be fine)
 // if designPossible fails return to last success (I think that one is broken)
-func designPossible(design string, patterns *Patterns) bool {
-	for _, pattern := range possiblePatterns(design, patterns) {
+func designPossible(design string, patterns *Patterns, depth int) bool {
+	status := false
+
+	for _, pattern := range getPossiblePatterns(design, patterns) {
 		nextDesign := strings.TrimPrefix(design, pattern)
 		if nextDesign == "" {
 			return true
 		}
-		if isSuccess := designPossible(nextDesign, patterns); isSuccess {
+		if possible := designPossible(nextDesign, patterns, depth+1); possible {
 			return true
 		}
 	}
 
-	patterns.append(design)
-	return false
+	patterns.appendToBlacklist(design)
+	return status
 }
 
-func possiblePatterns(design string, patterns *Patterns) (contains []string) {
+func getPossiblePatterns(design string, patterns *Patterns) (contains []string) {
 	if isImpossibleDesign(design, patterns.blacklist) {
 		return
 	}
@@ -168,4 +164,34 @@ func isImpossibleDesign(design string, blacklist []string) bool {
 	}
 
 	return false
+}
+
+//////// Part 2 implementation ////////
+
+func calculatePossibilitiesForAll(patterns *Patterns) Patterns {
+
+	for _, value := range patterns.whitelist {
+		patterns.possibilities += calculatePossibilities(value, patterns, 0)
+	}
+
+	fmt.Printf("Combinations of possible designs: %d\n", patterns.possibilities)
+	return *patterns
+}
+
+func calculatePossibilities(design string, patterns *Patterns, depth int) int {
+	if _, in := patterns.cache[design]; in {
+		return patterns.cache[design]
+	}
+
+	for _, pattern := range getPossiblePatterns(design, patterns) {
+		if strings.HasPrefix(design, pattern) {
+			if nextDesign := strings.TrimPrefix(design, pattern); nextDesign == "" {
+				patterns.cache[design] = patterns.cache[design] + 1
+			} else {
+				patterns.cache[design] = patterns.cache[design] + calculatePossibilities(nextDesign, patterns, depth+1)
+			}
+		}
+	}
+
+	return patterns.cache[design]
 }
